ca3abe1e7d8469f0c8c778c0583656e47cba8d2c
Marmin <marmin.dev@gmail.com>
2022-12-07 04:48:33 +0900

    start project

---
+++ src/main/java/com/springdemo/marmindemo/MarmindemoApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.marmindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdemo/marmindemo/MarmindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.marmindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
